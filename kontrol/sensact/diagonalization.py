00d5d0f7dfd286b0509c1563429cd8c066fb5fa9
terrencetec <58246229+terrencetec@users.noreply.github.com>
2020-07-13 08:43:12 +0800

    Update diagonalization.py

--- kontrol/sensact/diagonalization.py
+++ kontrol/sensact/diagonalization.py
@@ -2,6 +2,9 @@
 """
 import numpy as np
 
-def model_reference_diagonalization(
+# def model_reference_diagonalization(
+#
+#         ):
 
-        ):
+def actuation_diagonalization(actuation_matrix, coupling_matrix):
+    actuation_matrix = np.array(actuation_matrix)
