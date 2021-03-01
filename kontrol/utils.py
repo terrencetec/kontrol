a51e80980da25d8c144bb1146723fee89567d48d
terrencetec <terrencetec@gmail.com>
2021-03-02 03:34:37 +0800

    utility functions for converting unstable transfer functions

--- kontrol/utils.py
+++ kontrol/utils.py
@@ -236,3 +236,65 @@ def zpk(zeros, poles, gain, unit='Hz', negate=True):
         zpk_tf *= tf([1], [1/p, 1])
 
     return zpk_tf
+
+
+def convert_unstable_tf(control_tf):
+    """Convert transfer function with unstable zeros and poles to tf without.
+
+    Parameters
+    ----------
+    control_tf: control.xferfcn.TransferFunction
+        The transfer function to be converted.
+
+    Returns
+    -------
+    tf_new: control.xferfcn.TransferFunction
+        control_tf with unstable zeros and poles flipped.
+
+    Note
+    ----
+    Warning can occur when the order gets high, e.g. 100.
+    This happens due to numerical accuracy,
+    i.e. coefficients get astronomically high when order gets high.
+    """
+    zeros_unstable = control_tf.zero()
+    poles_unstable = control_tf.pole()
+    zeros_stable = []
+    poles_stable = []
+    for zero in zeros_unstable:
+        # Ignore zeros/poles that have negative imaginary part.
+        if zero.imag != 0 and zero.imag < 0:
+            pass
+        elif zero.real > 0:
+            zeros_stable.append(-zero.real + 1j*zero.imag)
+        else:
+            zeros_stable.append(zero.real + 1j*zero.imag)
+    for pole in poles_unstable:
+        # Ignore zeros/poles that have negative imaginary part.
+        if pole.imag != 0 and pole.imag < 0:
+            pass
+        elif pole.real > 0:
+            poles_stable.append(-pole.real + 1j*pole.imag)
+        else:
+            poles_stable.append(pole.real + 1j*pole.imag)
+
+    tf_new = control.tf([1], [1])
+    s = control.tf("s")
+    for zero in zeros_stable:
+        if zero.imag != 0:
+            wn = np.sqrt(zero.real**2 + zero.imag**2)
+            zeta = -zero.real/wn
+            tf_new /= wn**2 / (s**2+2*zeta*wn*s+wn**2)
+        else:
+            tf_new *= s-zero.real
+    for pole in poles_stable:
+        if pole.imag != 0:
+            wn = np.sqrt(pole.real**2 + pole.imag**2)
+            zeta = -pole.real/wn
+            tf_new *= wn**2 / (s**2+2*zeta*wn*s+wn**2)
+        else:
+            tf_new /= s-pole.real
+
+    tf_new *= float(control_tf.dcgain())/float(tf_new.dcgain())
+
+    return tf_new
