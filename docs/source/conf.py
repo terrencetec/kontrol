b7490a1701483a6c82ffa0c0319a95929d83cb4f
terrencetec <terrencetec@gmail.com>
2021-03-26 00:57:17 +0800

    docs copyrights

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -18,7 +18,7 @@ sys.path.insert(0, os.path.abspath('../..'))
 # -- Project information -----------------------------------------------------
 
 project = 'Python Kontrol Library'
-copyright = '2020, TSANG Terrence Tak Lun'
+copyright = '2020-2021, TSANG Terrence Tak Lun'
 author = 'TSANG Terrence Tak Lun'
 
 # The full version, including alpha/beta/rc tags
