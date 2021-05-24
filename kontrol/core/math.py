30cd90b256933878f0ec0218a445dbef2fc4ded9
terrencetec <terrencetec@gmail.com>
2021-05-25 03:12:04 +0800

    kontrol.core.math.log_mse handles zero value, tests.

--- kontrol/core/math.py
+++ kontrol/core/math.py
@@ -26,7 +26,7 @@ def mse(x1, x2, weight=None):
     return np.mean(np.square(np.subtract(x2, x1)*weight))
 
 
-def log_mse(x1, x2, weight=None):
+def log_mse(x1, x2, weight=None, small_multiplier=1e-6):
     """Logarithmic mean square error/Mean square log error
 
     Parameters
@@ -37,13 +37,25 @@ def log_mse(x1, x2, weight=None):
         Array
     weight: array or None, optional
         weighting function.
-        If None, defaults to np.ones_like(x1)
+        If None, defaults to `np.ones_like(x1)`
+    small_multiplier: float, optional
+        `x1` will be modified by `x1`+`small_multiplier`*`np.min(x2)` if
+        0 exists in `x1`.
+        Same goes to `x2`.
+        If 0 both exists in `x1` and `x2`, raise.
 
     Returns
     -------
     float
         Logarithmic mean square error between arrays x1 and x2.
     """
+    if 0 in x1 and 0 in x2:
+        raise ValueError("zero value exists in both 'x1' and 'x2'"
+                         "Can't take np.log10()")
+    elif 0 in x1:
+        x1 += small_multiplier * np.min(x2)
+    elif 0 in x2:
+        x2 += small_multiplier * np.min(x1)
     return mse(x1=np.log10(x1), x2=np.log10(x2), weight=weight)
 
 
--- tests/core/test_math.py
+++ tests/core/test_math.py
@@ -18,7 +18,20 @@ def test_log_mse():
     a = np.array([1, 10, 100, 1000, 10000])
     b = np.array([10, 100, 1000, 10000, 100000])
     test = kontrol.core.math.log_mse(a, b)
-    assert correct == test
+    test_cal = correct==test
+
+    # Test handling of zero value array
+    correct=0
+    a = np.zeros(10)
+    b = np.ones(10)
+    test_zero_value_array_ab = kontrol.core.math.log_mse(
+        a, b, small_multiplier=1)
+    test_zero_value_array_ba = kontrol.core.math.log_mse(
+        b, a, small_multiplier=1)
+    test_zero_handle = (correct==test_zero_value_array_ab and
+                        correct==test_zero_value_array_ba)
+
+    assert np.all([test_cal, test_zero_handle])
 
 
 def test_quad_sum():
