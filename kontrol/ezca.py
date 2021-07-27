9f2bbc6e1e84943965803410861d6f0c8aca0a17
terrencetec <terrencetec@gmail.com>
2021-07-27 22:54:45 +0800

    ezca read and write matrices

--- kontrol/ezca.py
+++ kontrol/ezca.py
@@ -11,7 +11,7 @@ class Ezca(ezca.Ezca):
         """Constructor
 
         Parameters
-        ==========
+        ----------
         prefix : str
             Channel prefix. E.g. "K1:VIS-BS".
             "K1:" is implied in KAGRA DGS so "VIS-BS" is equivalent to
@@ -22,30 +22,31 @@ class Ezca(ezca.Ezca):
         super().__init__(prefix, timeout=timeout)
 
     def get_matrix(self, matrix, row_slicers=None, column_slicers=None):
-        """Returns a matrix from the EZCA record.
+        """Returns a matrix from the EPICS record using Ezca.
 
         Parameters
-        ==========
+        ----------
         matrix : str
             The matrix to be accessed.
         row_slicers : int, (int, int) or None, optional
             Row slicers, the rows to be accessed. Counting from 1.
             If an int is specified, then the number of rows to be accessed
             will be this number, starting from the [1, 1] element.
-            If a submatrix is to be accessed, the specify as a tuple.
+            If a submatrix is to be accessed, then specify as a tuple.
             Defaults None. If None, sets to full matrix.
         column_slicers : int, (int, int) or None, optional
             Column slicers, the columns to be accessed. Counting from 1.
             If an int is specified, then the number of columns to be accessed
             will be this number, starting from the [1, 1] element.
-            If a submatrix is to be accessed, the specify as a tuple.
+            If a submatrix is to be accessed, then specify as a tuple.
             Defaults None. If None, sets to full matrix.
 
         Returns
-        =======
+        -------
         numpy.array
             The accessed matrix.
         """
+        # Read the matrix until error to get the number of rows and columns.
         nrow, ncol = self._get_row_column(matrix)
         if isinstance(row_slicers, int):
             row_slicers = (1, row_slicers)
@@ -55,7 +56,7 @@ class Ezca(ezca.Ezca):
             column_slicers = (1, column_slicers)
         elif column_slicers is None:
             column_slicers = (1, ncol)
-            
+
         ezca_matrix = np.zeros((nrow, ncol))  # Initialize matrix placeholder
         for i in range(len(ezca_matrix)):
             for j in range(len(ezca_matrix)):
@@ -64,6 +65,77 @@ class Ezca(ezca.Ezca):
 
         return ezca_matrix[row_slicers[0]-1:row_slicers[1],
                            column_slicers[0]-1:column_slicers[1]]
+
+    def put_matrix(self, numpy_array, matrix,
+                   row_slicers=None, column_slicers=None):
+        """Put a matrix into the EPICS record using Ezca.
+
+        Parameters
+        ----------
+        numpy_array : array
+            The matrix values to be put.
+        matrix : str
+            The matrix in EPICS record.
+        row_slicers : int, (int, int) or None, optional
+            Row slicers, the rows to be accessed. Counting from 1.
+            If an int is specified, then the number of rows to be accessed
+            will be this number, starting from the [1, 1] element.
+            If a submatrix is to be accessed, then specify as a tuple.
+            The size determined by the tuple must match the size of
+            `numpy_array`
+            Defaults None. If None, sets to full matrix.
+        column_slicers : int, (int, int) or None, optional
+            Column slicers, the columns to be accessed. Counting from 1.
+            If an int is specified, then the number of columns to be accessed
+            will be this number, starting from the [1, 1] element.
+            If a submatrix is to be accessed, then specify as a tuple.
+            The size determined by the tuple must match the size of
+            `numpy_array`
+            Defaults None. If None, sets to full matrix.
+        """
+        # Read the matrix until error to get the number of rows and columns.
+        nrow, ncol = self._get_row_column(matrix)
+        if isinstance(row_slicers, int):
+            row_slicers = (1, row_slicers)
+        elif row_slicers is None:
+            row_slicers = (1, nrow)
+        if isinstance(column_slicers, int):
+            column_slicers = (1, column_slicers)
+        elif column_slicers is None:
+            column_slicers = (1, ncol)
+
+        if row_slicers[1] - row_slicers[0] + 1 != len(numpy_array):
+            raise ValueError("slicers must match the size of numpy_array.")
+        elif column_slicers[1] - column_slicers[0] + 1 != len(numpy_array[0]):
+            raise ValueError("slicers must match the size of numpy_array.")
+
+        epics_matrix = self.get_matrix(matrix)
+        epics_matrix[row_slicers[0]-1:row_slicers[1]-1,
+                     column_slicers[0]-1: column_slicers[1]-1] = numpy_array
+
+        self._put_matrix(numpy_array=epics_matrix, matrix=matrix)
+
+    def _put_matrix(self, numpy_array, matrix):
+        """Put a matrix into the EPICS record using Ezca, (low level function).
+
+        Parameters
+        ----------
+        numpy_array : array
+            The matrix values to be put.
+            Matrix size must match that in the EPICS record.
+        matrix : str
+            The matrix in EPICS record.
+        """
+        nrow, ncol = self._get_row_column(matrix)
+        if len(numpy_array) != nrow or len(numpy_array[0] != ncol):
+            raise ValueError("Size of numpy_array not equal to that in the"
+                             " EPICS record.")
+
+        for i in range(len(numpy_array)):
+            for j in range(len(numpy_array[i])):
+                self.write("{}_{}_{}".format(matrix, i+1, j+1),
+                           numpy_array[i, j])
+
 
     def _get_row_column(self, matrix):
         """Get the number of rows and columns of a matrix.
